4b6fb6c8edba12ea5fe004234313194bf4521267
karpinski-j <karpinskikuba389@gmail.com>
2023-11-12 11:37:34 +0100

    billing account

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -3,7 +3,7 @@ provider "google" {
   region  = var.region
 }
 terraform {
-  required_version = "~> 1.6.3"
+  required_version = "~> 1.5.0"
   required_providers {
     google = {
       version = "~> 4.84.0"
